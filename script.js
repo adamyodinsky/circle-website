cace17d3b656a9d29b7da743c3e571a3332f7b68
adamyodinsky <27074934+adamyodinsky@users.noreply.github.com>
2025-07-18 15:25:37 +0700

    test

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ document.addEventListener("DOMContentLoaded", function () {
   connectButton.textContent = `Connect with ${inviterName}`;
   connectButton.addEventListener("click", function () {
     // Construct deep links
-    const androidDeepLink = `com.circle://(screens)/profile/${userId}`;
-    const iosDeepLink = `com.circle://(screens)/profile/${userId}`;
+    const androidDeepLink = `com.circle://profile/${userId}`;
+    const iosDeepLink = `com.circle://profile/${userId}`;
 
     // Fallback URLs (app store links)
     const androidStoreLink =
